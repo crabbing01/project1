e0ef05297c7fa3b09a103e3f045f1701877b2339
crabbing01 <49535122+crabbing01@users.noreply.github.com>
2019-04-11 15:53:22 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -127,6 +127,7 @@ function check() {
     }
 }
 
-app.listen(3000, function() {
-    console.log('Listening on port 3000!');
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Listening!');
 });
+
